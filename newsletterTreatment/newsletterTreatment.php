b94af51c810c543a9779827000a8b39e40d8c7e8
GELJORF <geljorf72@gmail.com>
2023-11-08 16:20:36 +0100

    Traitement de la newsletter, en cours

--- newsletterTreatment/newsletterTreatment.php
+++ newsletterTreatment/newsletterTreatment.php
@@ -1,39 +1,39 @@
 <?php
+require_once __DIR__ . "/../lessonOnLine/functions.php";
 
-require_once __DIR__ ."/../lessonOnLine/functions.php";
 
 $email = $_POST['email'];
 
-// Vérification de la validité de l'adresse e-mail
+
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     echo 'L\'adresse e-mail est invalide.';
     exit();
 }
 
-// Vérification de l'existence de l'adresse e-mail dans la table
+
 $sql = 'SELECT id FROM newsletter_subscribers WHERE email = :email';
 $stmt = $db->prepare($sql);
 $stmt->bindParam(':email', $email);
 $stmt->execute();
 
-// Si l'adresse e-mail existe déjà, on ne fait rien
 if ($stmt->rowCount() > 0) {
     echo 'L\'adresse e-mail est déjà inscrite à la newsletter.';
     exit();
 }
 
-// Ajout de l'adresse e-mail à la table
+
 $sql = 'INSERT INTO newsletter_subscribers (email) VALUES (:email)';
 $stmt = $db->prepare($sql);
 $stmt->bindParam(':email', $email);
 $stmt->execute();
 
-// Envoi d'un e-mail de confirmation
-$subject = 'Confirmation d\'inscription à la newsletter';
-$message = 'Merci de votre inscription à notre newsletter.';
-mail($email, $subject, $message);
 
-// Affichage d'un message de confirmation
+$subscribersFile = __DIR__ . "/subscribers-list.txt";
+$file = fopen($subscribersFile, "a");
+fwrite($file, $email . "\n");
+fclose($file);
+
+
 echo 'Vous êtes inscrit à la newsletter.';
-
+header('Location: ../index.php');
 ?>
--- teachingMethods.php
+++ teachingMethods.php
@@ -71,7 +71,7 @@
 <div style="background-color: #87CEEB; margin: 0 auto; max-width: 400px; padding: 20px; text-align: center;">
     <h2 class="text-2xl font-semibold" style="margin-bottom: 10px;">Inscrivez-vous à notre lettre d'information !</h2>
     <form action="newsletterTreatment/newsletterTreatment.php" method="post">
-        <input type="email" name="email" placeholder="Votre courriel" style="margin-right: 10px; padding: 10px;" required>
+        <input type="email" name="email" id="email" placeholder="Votre courriel" style="margin-right: 10px; padding: 10px;" required>
         <input type="submit" name="subscribe" value="S'inscrire" style="background-color: #1E90FF; color: #fff; padding: 10px; border: none; border-radius: 5px;">
     </form>
 </div>
