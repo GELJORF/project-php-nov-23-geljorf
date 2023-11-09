034648d90553f1cf2d1c40002b9c37238d3ff6c3
GELJORF <geljorf72@gmail.com>
2023-11-09 12:15:25 +0100

    Corriger le code de la newsletter

--- newsletterTreatment/index.php
+++ newsletterTreatment/index.php
@@ -10,7 +10,7 @@
 <body class="bg-blue-200 flex justify-center items-center h-screen">
     <div class="bg-blue-300 max-w-md p-8 rounded-lg text-center">
         <h2 class="text-2xl font-semibold mb-4">Inscrivez-vous à notre lettre d'information !</h2>
-        <form action="index.php" method="post" class="flex flex-col space-y-4">
+        <form action="newsletterTreatment.php" method="post" class="flex flex-col space-y-4">
             <input type="email" name="email" id="email" placeholder="Votre courriel" class="px-4 py-2 border rounded">
             <button type="submit" name="subscribe" class="bg-blue-500 text-white px-4 py-2 rounded">S'inscrire</button>
         </form>
--- newsletterTreatment/newsletterTreatment.php
+++ newsletterTreatment/newsletterTreatment.php
@@ -1,57 +1,31 @@
 <?php
 require_once __DIR__ . "/../lessonOnLine/functions.php";
+$db = connectToDatabase();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
 
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo 'L\'adresse e-mail est invalide.';
-        header('Location: ../index.php');
-        exit();
+    try {
+        $sql = 'INSERT INTO newsletter_subscribers (email_adresse) VALUES (:email)';
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+        echo 'L\'adresse e-mail a été ajoutée à la base de données.';
+
+        $subscribersFile = __DIR__ . "/subscribers-list.txt";
+        $file = fopen($subscribersFile, "a");
+
+        if ($file === false) {
+            echo 'Erreur lors de l\'ouverture du fichier texte.';
+        } else {
+            fwrite($file, $email . "\n");
+            fclose($file);
+            echo 'L\'adresse e-mail a été ajoutée au fichier texte.';
+        }
+        echo '<meta http-equiv="refresh" content="3;url=../index.php">';
+
+    } catch (PDOException $e) {
+        echo 'Erreur PDO : ' . $e->getMessage();
     }
-
-    $sql = 'SELECT ID FROM newsletter_subscribers WHERE email_adresse = :email';
-    $stmt = $db->prepare($sql);
-    $stmt->bindParam(':email', $email);
-    $stmt->execute();
-
-    if ($stmt->errorInfo()[0] !== '00000') {
-        echo 'Erreur SQL lors de la vérification de l\'adresse e-mail dans la base de données : ' . implode(', ', $stmt->errorInfo());
-        header('Location: ../index.php');
-        exit();
-    }
-
-    if ($stmt->rowCount() > 0) {
-        echo 'L\'adresse e-mail est déjà inscrite à la newsletter.';
-        header('Location: ../index.php');
-        exit();
-    }
-
-    $sql = 'INSERT INTO newsletter_subscribers (email_adresse) VALUES (:email)';
-    $stmt = $db->prepare($sql);
-    $stmt->bindParam(':email', $email);
-    $stmt->execute();
-
-    if ($stmt->errorInfo()[0] !== '00000') {
-        echo 'Erreur SQL lors de l\'insertion de l\'adresse e-mail dans la base de données : ' . implode(', ', $stmt->errorInfo());
-        header('Location: ../index.php');
-        exit();
-    }
-
-    $subscribersFile = __DIR__ . "/subscribers-list.txt";
-    $file = fopen($subscribersFile, "a");
-
-    if ($file === false) {
-        echo 'Erreur lors de l\'ouverture du fichier texte.';
-        header('Location: ../index.php');
-        exit();
-    }
-
-    fwrite($file, $email . "\n");
-    fclose($file);
-
-    echo 'Vous êtes inscrit à la newsletter.';
-    header('Location: ../index.php');
-    exit();
 }
 ?>
