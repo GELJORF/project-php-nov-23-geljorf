a78bb7e38e53fc66bb7790167650a4f8b7934eef
GELJORF <geljorf72@gmail.com>
2023-11-09 10:18:48 +0100

    Créer une page indépendante pour la lettre d'information. Réorganiser la disposition des pages de la barre de navigation

--- components/nav.php
+++ components/nav.php
@@ -43,6 +43,9 @@ require_once __DIR__ . "/../navbarTreatment.php";
         <li>
           <a href="../contact/contact.php" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover-bg-gray-100 md:hover-bg-transparent md:border-0 md:hover-text-blue-700 md:p-0 dark:text-white md:dark:hover-text-blue-500 dark:hover-bg-gray-700 dark:hover-text-white md:dark:hover-bg-transparent">Contact</a>
         </li>
+        <li>
+          <a href="../newsletterTreatment/index.php" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover-bg-gray-100 md:hover-bg-transparent md:border-0 md:hover-text-blue-700 md:p-0 dark:text-white md:dark:hover-text-blue-500 dark:hover-bg-gray-700 dark:hover-text-white md:dark:hover-bg-transparent">Lettre d'information</a>
+        </li>
       </ul>
     </div>
   </div>
---
+++ newsletterTreatment/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Newsletter</title>
+</head>
+<body>
+<div style="background-color: #87CEEB; margin: 0 auto; max-width: 400px; padding: 20px; text-align: center;">
+    <h2 class="text-2xl font-semibold" style="margin-bottom: 10px;">Inscrivez-vous à notre lettre d'information !</h2>
+    <form action="index.php" method="post">
+        <input type="email" name="email" id="email" placeholder="Votre courriel" style="margin-right: 10px; padding: 10px;" required>
+        <input type="submit" name="subscribe" value="S'inscrire" style="background-color: #1E90FF; color: #fff; padding: 10px; border: none; border-radius: 5px;">
+    </form>
+</div>
+</body>
+</html>
+
