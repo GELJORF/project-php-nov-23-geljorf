55c77557f6b0c9d947d56af59dc83af04fde3bc7
GELJORF <geljorf72@gmail.com>
2023-11-09 11:15:24 +0100

    Modifier le footer

--- newsletterTreatment/index.php
+++ newsletterTreatment/index.php
@@ -4,15 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Newsletter</title>
+    <script src="https://cdn.tailwindcss.com?plugins=typography"></script>
 </head>
 <body>
-<div style="background-color: #87CEEB; margin: 0 auto; max-width: 400px; padding: 20px; text-align: center;">
-    <h2 class="text-2xl font-semibold" style="margin-bottom: 10px;">Inscrivez-vous à notre lettre d'information !</h2>
-    <form action="index.php" method="post">
-        <input type="email" name="email" id="email" placeholder="Votre courriel" style="margin-right: 10px; padding: 10px;" required>
-        <input type="submit" name="subscribe" value="S'inscrire" style="background-color: #1E90FF; color: #fff; padding: 10px; border: none; border-radius: 5px;">
-    </form>
-</div>
+<body class="bg-blue-200 flex justify-center items-center h-screen">
+    <div class="bg-blue-300 max-w-md p-8 rounded-lg text-center">
+        <h2 class="text-2xl font-semibold mb-4">Inscrivez-vous à notre lettre d'information !</h2>
+        <form action="index.php" method="post" class="flex flex-col space-y-4">
+            <input type="email" name="email" id="email" placeholder="Votre courriel" class="px-4 py-2 border rounded">
+            <button type="submit" name="subscribe" class="bg-blue-500 text-white px-4 py-2 rounded">S'inscrire</button>
+        </form>
+    </div>
 </body>
 </html>
 
--- newsletterTreatment/newsletterTreatment.php
+++ newsletterTreatment/newsletterTreatment.php
@@ -1,39 +1,57 @@
 <?php
 require_once __DIR__ . "/../lessonOnLine/functions.php";
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
 
-$email = $_POST['email'];
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo 'L\'adresse e-mail est invalide.';
+        header('Location: ../index.php');
+        exit();
+    }
 
+    $sql = 'SELECT ID FROM newsletter_subscribers WHERE email_adresse = :email';
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':email', $email);
+    $stmt->execute();
 
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    echo 'L\'adresse e-mail est invalide.';
+    if ($stmt->errorInfo()[0] !== '00000') {
+        echo 'Erreur SQL lors de la vérification de l\'adresse e-mail dans la base de données : ' . implode(', ', $stmt->errorInfo());
+        header('Location: ../index.php');
+        exit();
+    }
+
+    if ($stmt->rowCount() > 0) {
+        echo 'L\'adresse e-mail est déjà inscrite à la newsletter.';
+        header('Location: ../index.php');
+        exit();
+    }
+
+    $sql = 'INSERT INTO newsletter_subscribers (email_adresse) VALUES (:email)';
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam(':email', $email);
+    $stmt->execute();
+
+    if ($stmt->errorInfo()[0] !== '00000') {
+        echo 'Erreur SQL lors de l\'insertion de l\'adresse e-mail dans la base de données : ' . implode(', ', $stmt->errorInfo());
+        header('Location: ../index.php');
+        exit();
+    }
+
+    $subscribersFile = __DIR__ . "/subscribers-list.txt";
+    $file = fopen($subscribersFile, "a");
+
+    if ($file === false) {
+        echo 'Erreur lors de l\'ouverture du fichier texte.';
+        header('Location: ../index.php');
+        exit();
+    }
+
+    fwrite($file, $email . "\n");
+    fclose($file);
+
+    echo 'Vous êtes inscrit à la newsletter.';
+    header('Location: ../index.php');
     exit();
 }
-
-
-$sql = 'SELECT id FROM newsletter_subscribers WHERE email = :email';
-$stmt = $db->prepare($sql);
-$stmt->bindParam(':email', $email);
-$stmt->execute();
-
-if ($stmt->rowCount() > 0) {
-    echo 'L\'adresse e-mail est déjà inscrite à la newsletter.';
-    exit();
-}
-
-
-$sql = 'INSERT INTO newsletter_subscribers (email) VALUES (:email)';
-$stmt = $db->prepare($sql);
-$stmt->bindParam(':email', $email);
-$stmt->execute();
-
-
-$subscribersFile = __DIR__ . "/subscribers-list.txt";
-$file = fopen($subscribersFile, "a");
-fwrite($file, $email . "\n");
-fclose($file);
-
-
-echo 'Vous êtes inscrit à la newsletter.';
-header('Location: ../index.php');
 ?>
