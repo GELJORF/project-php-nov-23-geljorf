51850c3807714ec16860ae61d6ddce9d29898fc7
GELJORF <geljorf72@gmail.com>
2023-11-10 14:29:59 +0100

    Connexion

--- connexion/login.php
+++ connexion/login.php
@@ -3,9 +3,9 @@
 <?php
 session_start();
 
-// Vérifiez si l'utilisateur a soumis le formulaire de connexion
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Assurez-vous d'avoir établi une connexion PDO à votre base de données
+    
     try {
         $pdo = new PDO(
             $dsn = "mysql:host=host.docker.internal;port=3306;dbname='hb_pdo_pe7';charset=utf8mb4",
@@ -16,11 +16,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         exit("Erreur de connexion à la base de données : " . $e->getMessage());
     }
 
-    // Collectez les informations d'identification de l'utilisateur
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    // Effectuez une requête SQL pour vérifier les informations d'identification
+    
     $sql = 'INSERT * INTO connected WHERE email = :email';
     $stmt = $pdo->prepare($sql);
     $stmt->bindValue(':email', $email);
@@ -28,12 +27,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $user = $stmt->fetch();
 
     if ($user && password_verify($password, $user['password'])) {
-        // Les informations d'identification sont valides, l'utilisateur est connecté avec succès
+        
         $_SESSION['user_id'] = $user['ID'];
         $_SESSION['user_name'] = $user['name'];
         $_SESSION['user_email'] = $user['email'];
 
-        // Redirigez l'utilisateur vers la page de tableau de bord appropriée en fonction de son type d'utilisateur (professeur ou étudiant)
         if ($user['user_type'] === 'professor') {
             header('Location: dashboard.php?type=professor');
         } else {
@@ -43,7 +41,6 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         echo 'Identifiants de connexion non valides.';
     }
 } else {
-    // Gestion des accès directs à ce script sans soumission de formulaire
     echo 'Accès non autorisé.';
 }
 ?>
