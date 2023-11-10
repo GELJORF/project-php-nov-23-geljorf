b21100af75dfc0f4a63b3dad3af7d524d0100675
GELJORF <geljorf72@gmail.com>
2023-11-10 10:27:27 +0100

    Modifier lessons.php

--- lessonOnLine/lessons.php
+++ lessonOnLine/lessons.php
@@ -1,16 +1,16 @@
-<!-- Le code de cette page est mon premier essai de coder la page de cours, en mode statique,
+<!-- Le code de cette page est mon premier essai de coder la page de Leçon, en mode statique,
 je l'ai gardée afin de laisser une trace du raisonnement qui m'a conduit vers les nouvelles pages -->
 
 <?php
 $lesson = [
-    ["id" => 1, "title" => "Cours 1 - Alphabet"],
-    ["id" => 2, "title" => "Cours 2 - Articulation des lettres arabes"],
-    ["id" => 3, "title" => "Cours 3 - Orthographes des lettres arabes"],
-    ["id" => 4, "title" => "Cours 4 - Se présenter en arabe"],
-    ["id" => 5, "title" => "Cours 5 - Premiers pas grammaticaux"],
-    ["id" => 6, "title" => "Cours 6 - Pronoms personnels"],
-    ["id" => 7, "title" => "Cours 7 - Verbe avoir"],
-    ["id" => 8, "title" => "Cours 8 - Verbe être"]
+    ["id" => 1, "title" => "Leçon 1 - Alphabet"],
+    ["id" => 2, "title" => "Leçon 2 - Articulation des lettres arabes"],
+    ["id" => 3, "title" => "Leçon 3 - Orthographes des lettres arabes"],
+    ["id" => 4, "title" => "Leçon 4 - Se présenter en arabe"],
+    ["id" => 5, "title" => "Leçon 5 - Premiers pas grammaticaux"],
+    ["id" => 6, "title" => "Leçon 6 - Pronoms personnels"],
+    ["id" => 7, "title" => "Leçon 7 - Verbe avoir"],
+    ["id" => 8, "title" => "Leçon 8 - Verbe être"]
 ];
 ?> 
 
