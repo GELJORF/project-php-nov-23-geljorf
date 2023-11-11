44b51bf7bcf1361b29e550e365463fc38f4c299d
GELJORF <geljorf72@gmail.com>
2023-11-11 19:50:09 +0100

    Corriger le code du script d'inscription

--- inscription/index.php
+++ inscription/index.php
@@ -1,10 +1,9 @@
 <?php
-session_start();
 
 function connectToDatabase() {
     try {
         $pdo = new PDO(
-            $dsn = "mysql:host=host.docker.internal;port=3306;dbname='hb_pdo_pe7';charset=utf8mb4",
+            $dsn = "mysql:host=host.docker.internal;port=3306;dbname=hb_pdo_pe7;charset=utf8mb4",
             $username ='hb_pdo_pe7',
             $password = 'TUEjdMVP1onaGMQF',
         );
@@ -18,31 +17,32 @@ function connectToDatabase() {
 
 $pdo = connectToDatabase();
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $name = $_POST['name'];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
     $email = $_POST['email'];
-    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
     $telephone = $_POST['telephone'];
-    $lettre_motivation = $_POST['lettre_motivation'];
     $level = $_POST['level'];
-    $user_type = $_POST['user_type'];
 
-    $sqlInsert = 'INSERT INTO connected (name, email, password, telephone, lettre_motivation, level, user_type) VALUES (:name, :email, :password, :telephone, :lettre_motivation, :level, :user_type)';
-    $stmtInsert = $pdo->prepare($sqlInsert);
-    $stmtInsert->bindValue(':name', $name);
-    $stmtInsert->bindValue(':email', $email);
-    $stmtInsert->bindValue(':password', $password);
-    $stmtInsert->bindValue(':telephone', $telephone);
-    $stmtInsert->bindValue(':lettre_motivation', $lettre_motivation);
-    $stmtInsert->bindValue(':level', $level);
-    $stmtInsert->bindValue(':user_type', $user_type);
 
-    if ($stmtInsert->execute()) {
-        header('Location: inscription_reussie.php');
-    } else {
-        echo 'Erreur lors de l\'inscription. Veuillez réessayer.';
-    }
-} else {
-    echo 'Accès non autorisé.';
+    $file = $_FILES['file']['name'];
+    $lettre_motivation = $_POST['lettre_motivation'];
+    $password = $_POST['password'];
+
+    
+    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+
+    
+    $insertedData = "Nom: $last_name, Prénom: $first_name, Email: $email, Téléphone: $telephone, Niveau: $level";
+
+    
+    $file = fopen("new_subscribers.txt", "a");
+    fwrite($file, $insertedData . PHP_EOL);
+    fclose($file);
+
+    echo "Inscription réussie ! Données de l'inscrit : " . $insertedData;
 }
+
 ?>
--- inscription/inscription.php
+++ inscription/inscription.php
@@ -12,8 +12,12 @@
     <h1 class="text-4xl font-bold text-blue-600 mb-4 mt-8">Inscription</h1>
     <form class="bg-white p-8 rounded-lg shadow-md w-full max-w-md" action="index.php" method="POST" enctype="multipart/form-data">
         <div class="mb-4">
-            <label class="text-gray-700 text-sm" for="name">Nom de l'utilisateur :</label>
-            <input class="border border-blue-300 rounded p-2 w-full" type="text" id="name" name="name" required>
+            <label class="text-gray-700 text-sm" for="first_name">Prénom :</label>
+            <input class="border border-blue-300 rounded p-2 w-full" type="text" id="first_name" name="first_name" required>
+        </div>
+        <div class="mb-4">
+            <label class="text-gray-700 text-sm" for="last_name">Nom :</label>
+            <input class="border border-blue-300 rounded p-2 w-full" type="text" id="last_name" name="last_name" required>
         </div>
         <div class="mb-4">
             <label class="text-gray-700 text-sm" for="email">Email :</label>
@@ -46,13 +50,7 @@
                 <option value="intermediate">Intermédiaire</option>
                 <option value="advanced">Avancé</option>
             </select>
-        </div>
-        <div class="mb-4">
-            <label class="text-gray-700 text-sm" for="user_type">Type d'utilisateur :</label>
-            <select class="border border-blue-300 rounded p-2 w-full" id="user_type" name="user_type">
-                <option value="student">Élève</option>
-                <option value="professor">Professeur</option>
-            </select>
+
         </div>
         <button class="bg-blue-600 text-white p-2 rounded hover:bg-blue-700 w-full" type="submit">Inscription</button>
     </form>
