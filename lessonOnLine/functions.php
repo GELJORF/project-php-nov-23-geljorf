f79da3cfbf80e0b8179388d5570029ba9e0f4705
GELJORF <geljorf72@gmail.com>
2024-01-15 20:32:05 +0100

    lessons DB gitignore

--- lessonOnLine/functions.php
+++ lessonOnLine/functions.php
@@ -1,13 +1,9 @@
 <?php
+include_once 'config.php'; 
 function connectToDatabase()
-            {
-            
+            {           
                 try {
-                    $pdo = new PDO(
-                        $dsn = "mysql:host=host.docker.internal;port=3306;dbname=lessons;charset=utf8mb4",
-                        $username = 'Gazi',
-                        $password = 'C6UvGSR/M_XFDcP@'
-                    );
+                    $pdo = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD, );
             
                     return $pdo;
                 } catch (PDOException $e) {
